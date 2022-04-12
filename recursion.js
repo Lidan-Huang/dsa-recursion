915f4ade418eea5939825cba1e7f7d1f5aa24275
edwinkim97 <edwinkim124@gmail.com>
2022-04-12 12:34:43 -0700

    finished everyOther function recursively

--- recursion.js
+++ recursion.js
@@ -22,7 +22,9 @@ function longest(words) {
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
+  if (str.length === 0) return "";
 
+  return str[0] + everyOther(str.slice(2));
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
